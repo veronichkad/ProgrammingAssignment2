774227e82b543a50d268712899975e6567d7f779
Veronika Devedzhieva <veronika@Veronikas-MacBook-Pro.local>
2015-07-25 22:49:15 +0200

    inverse and store in cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The file contains two functions - makeCacheMatrix and cacheSolve. The are used to create matrix,
+# calculate the inverse and store it in the cache
 
-## Write a short comment describing this function
+# MakeCacheMatrix is a function that is destined to store a martix and a cached value of the inverse of the
+# matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  
+  # theres is nothing to cache at the beginning so the variable cache is set to NULL
+  cache <- NULL
+  
+  # set value of the matrix
+  setMatrix <- function(value) {
+    x <<- value
+    cache <<- NULL
+  }
+  
+  # get value of the matrix
+  getMatrix <- function() {
+    x
+  }
+  
+  # cache the inverse
+  cacheInverse <- function(solve) {
+    cache <<- solve
+  }
+  
+  # get the cached inverse value
+  getInverse <- function() {
+    cache
+  }
+  
+  # return a list of all the elements of the matrix
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# The second function makeCacheMatrix - it  returns the inverse if it exists, otherwise - calculates it,
+# stores it in the cache and returns it. The matrix should be created with the function above.
+cacheSolve <- function(y, ...) {
+    
+  # get the cached value
+  inverse <- y$getInverse()
+  
+  # if there is already assigned value then return it
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  # otherwise caclulate the inverse and store it in the cache
+  data <- y$getMatrix()
+  inverse <- solve(data)
+  y$cacheInverse(inverse)
+  
+  # return the inverse
+  inverse
 }
